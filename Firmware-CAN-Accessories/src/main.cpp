4787b327b7c146d2f7eb13b04021361c2d077b8e
Evan <ebabbs23@gmail.com>
2025-11-19 20:38:24 -0800

    syntax fixes

--- Firmware-CAN-Accessories/src/main.cpp
+++ Firmware-CAN-Accessories/src/main.cpp
@@ -5,13 +5,13 @@ class Accessory {
   public:
     string Name;
     PinName Mosfet_gate;
-    int Can_ID;
+    int CAN_ID;
     bool Do_it_blink_question_mark;
 
-    Accessory(string name, PinName mosfet_gate, int CAN_ID, bool do_it_blink_question_mark) { //mosfet gate doubles as index
+    Accessory(string name, PinName mosfet_gate, int can_id, bool do_it_blink_question_mark) { //mosfet gate doubles as index
       Name = name;
       Mosfet_gate = mosfet_gate;
-      Can_ID = CAN_ID;
+      CAN_ID = can_id;
       Do_it_blink_question_mark = do_it_blink_question_mark;
     }
 };
@@ -30,8 +30,8 @@ Accessory extra2 = Accessory("extra2", PA_10, 7, 0);
 PinName gate_list[8] = {PB_4, PB_5, PA_8, PB_1, PB_6, PB_7, PB_0, PA_10}; //consistent with indices
 
 //INITIALIZATION
-int current_states[6] = {0, 0, 0, 0, 0, 0};
-int new_states[6] = {0, 0, 0, 0, 0, 0};
+int current_states[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+int new_states[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 int bits[8] = {0};
 
 //CAN INITIALIZATION
@@ -45,23 +45,24 @@ void can_message_received() {
   unsigned char operation_mode = incoming_message.data[0];
 
   switch (operation_mode) {
-    case (0):
+    case (0): {
       uint8_t data_byte = incoming_message.data[1];
       for (int i = 0; i<8; i++) {
         bool bit = (data_byte >> i) & 0x01;
         change_state(i, bit); //i=0 is accessory index 0 (headlights), i=7 is accessory index 7 (extra), going from left to right
-        break;
       }
-    
-    case (1):
+      break;
+    }
+    case (1): {
       //ain't nobody use toggle mode
       break;
-    
-    case (2):
+    }
+    case (2): {
       uint8_t data_byte = incoming_message.data[1];
       uint8_t id = data_byte >> 1;
       uint8_t set = data_byte & 0x01;
       break;
+    }
   }
 }
 
